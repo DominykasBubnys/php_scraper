e526a73f81f5eae8b07983773200c33bfb88ca42
dominykas <bubnys.it@gmail.com>
2022-07-22 16:23:42 +0300

    scraping some data

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -1,10 +1,22 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Goutte\Client;
 use Illuminate\Http\Request;
+use Psy\Util\Json;
 
 class ScraperController extends Controller
 {
     //
+
+    public function scraper(){
+
+        $client = new Client();
+        $url = "https://www.worldometers.info/coronavirus/";
+        $page = $client->request('GET', $url);
+        $virus_number = $page->filter(".maincounter-number")->text();
+
+        echo "<h1>number: {{$virus_number}}</h1>";
+        return view("scraper");
+    }
 }
